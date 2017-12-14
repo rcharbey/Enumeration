9e1fb2e036d53be16794f2a1bb694770025c3a46
Raphael <r.charbey@gmail.com>
2017-12-14 12:38:30 +0100

    test time

--- run_enumerations.sh
+++ run_enumerations.sh
@@ -2,5 +2,5 @@ TIMEFORMAT=%2U
 for ego in `cat egos_to_run` 
 do
 	echo $ego >> '../time_per_ego.csv'
-	{ time python main_enumeration.py $ego 4 ;} 2> '../time_per_ego.csv'
+	{ time python main_enumeration.py $ego 4 ;} 2>> '../time_per_ego.csv'
 done
