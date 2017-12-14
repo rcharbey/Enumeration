aa1358d84369abf6d80567b120dff08e71d3b075
Raphael <r.charbey@gmail.com>
2017-12-14 12:18:18 +0100

    test time

--- run_enumerations.sh
+++ run_enumerations.sh
@@ -1,6 +1,9 @@
+TIMEFORMAT=%2U
 for ego in `cat egos_to_run` 
 do
-	t=times python main_enumeration.py $ego 4
-	echo $t
+	t=time { {
+		python main_enumeration.py $ego 4;
+	} 2>&1; }
+
 	echo $ego $t >> '../time_per_ego.csv'
 done
